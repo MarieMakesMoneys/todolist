af7c13756605445e907c1b80aa92ddcf9864cff4
MarieMakesMoneys <marie.collecchia@gmail.com>
2022-12-02 10:50:14 +0100

    Set up function to display tasks in chronological order

--- src/timeline.js
+++ src/timeline.js
@@ -1,7 +1,11 @@
-import displayTasks from "./home.js"
+import { displayTasks } from "./home.js"
 import { allTasks } from "./index.js"
 
-export default function displayTimeline() {
-    allTasks.sort(function(a, b){return a.dueDate - b.dueDate})
-    displayTasks(allTasks)
+let sortedTasks;
+function displayTimeline() {
+    sortedTasks = [...allTasks]
+    sortedTasks.sort(function(a, b){return a.dueDate - b.dueDate})
+    displayTasks(sortedTasks)
 }
+
+export {sortedTasks, displayTimeline}
